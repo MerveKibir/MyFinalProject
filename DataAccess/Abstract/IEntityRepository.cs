58cf315a51bfce14e837ef1725c3c1089c3e4f94
MERVE KİBİR <merve.kibir78@gmail.com>
2021-02-04 00:37:55 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint (kısıt)= where T:class
+    //class=referans tip olabilir
+    //IEntity= IEntity veya onu implamente edebilen bir nesne olabilir
+    //new() : new lenebilir olmalı. IEntity olmasını önlemek için.
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);//ayrı ayrı şartlı metodlar yazılmasına gerek bırakmıyor
+        //filter=NULL; FİLTRESİZSE HEPSİNİ GETİR
+        //null değilse ona göre getir
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,88 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal() //crop+tap+tab
+        {
+            //Oracle,Sql Server'dan gelir gibi simüle edildi
+            _products = new List<Product> { //içinde ürünler olan bir list oluşturuldu
+                new Product {ProductID=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15}, //veri girişleri yapıldı
+                new Product {ProductID=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product {ProductID=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product {ProductID=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product {ProductID=5,CategoryId=2,ProductName="Mouse",UnitPrice=85,UnitsInStock=1}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            // _products.Remove(product); 
+            
+            //normalde bir listeden eleman siler ama burada direkt olmaz. 
+            //Çünkü beş adet veri beş adet adrese kaydoldu. Arayüzden bilgiler aynı olsa da o adres aynı değil.
+            //bu yüzden her birinde farklı olan Id özelliklerini kullanırız.
+
+            //    Product productToDelete = null;
+
+            //    foreach (var p in _products)  //tek tek bütün elemanları dolaşarak bulur.
+            //    {
+            //        if (product.ProductID == p.ProductID)
+            //        {
+            //            productToDelete = p;
+            //        }
+            //    }
+
+            //LINQ - Language Integrated Quary
+
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+            // foreach ın kısa hali yazılmış oldu.
+            //p takma isim.
+            //sonrası koşul kısmı.
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products; //liste olduğu gibi döndürüldü.
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+            //where koşulunu sağlayanları yeni bir liste yaparak döndürür.
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderilen ürün id sine sahip olan listedeki ürünü bul...
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
